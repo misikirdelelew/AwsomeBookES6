cf0e0a61979752e5e8c33bface2d24e99a89b2ad
MishDM <mish@g.com>
2023-02-22 10:03:47 -0800

    fix luxon.js import issue

--- modules/datetime.js
+++ modules/datetime.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line
-import { DateTime } from 'https://moment.github.io/luxon/es6/luxon.min.js';
-
+import { DateTime } from './luxon.js';
 const showdate = document.querySelector('#showdate');
-
 setInterval(() => {
   const now = DateTime.now();
   showdate.innerHTML = now.toFormat('ccc LLL dd yyyy, hh:mm:ss a');
